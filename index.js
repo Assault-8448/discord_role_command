e377a7519c439e1b8673b346bf35a5b9bb13c4d3
Assault <vvvvvgaga12356@yahoo.co.jp>
2021-04-01 17:09:55 +0900

    e

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const djs = require("discord.js");
+const client = new djs.Client();
+const prefix = "-";//ここ変えると/pingの/が変わる
 
-client.on('ready', () => {
-    console.log('準備完了！');
-});
+client.login("ODI3MDg2MzYzMjkwNTAxMTUy.YGV6Mw.LwcHYTXhjMcTzJmUEUYYxHBz458");
 
-client.on('message', message => {
-  if (message.content === '-a') {
-    message.channel.send('E');
+client.on("message", (message) => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+  if(message.channel.lock) return;
+  if(!message.content.match(new RegExp(`^${this.config.bot_config.prefix}|<@!?${client.user.id}>`))) return;
+
+  const isMention = (message.mentions.has(client.user.id)) ? `<@!${client.user.id}>` : this.config.bot_config.prefix;
+  const args = message.content.toString().slice(isMention.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  if(command === "no") {
+    message.channel.send("stop");
   }
 });
-
-client.login('ODI3MDg2MzYzMjkwNTAxMTUy.YGV6Mw.LwcHYTXhjMcTzJmUEUYYxHBz458');
