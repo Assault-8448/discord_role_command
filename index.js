beaa06552078e7840e8134f3f718f9e72d722997
Assault <vvvvvgaga12356@yahoo.co.jp>
2021-04-01 17:24:24 +0900

    uooooo

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const djs    = require("discord.js");
-const token  = require ('./config.json');
+const config = require ('./config.json');
 const client = new djs.Client();
-const prefix = "-";//ここ変えると/pingの/が変わる
+const prefix = "-";
 
-client.login(token);
+client.login(config.token);
+
+client.once('ready', () => {
+  console.log("E");
+});
 
 client.on("message", (message) => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
   if(message.channel.lock) return;
-  if(!message.content.match(new RegExp(`^${this.config.bot_config.prefix}|<@!?${client.user.id}>`))) return;
+  if(!message.content.match(new RegExp(`^${config.prefix}|<@!?${client.user.id}>`))) return;
 
-  const isMention = (message.mentions.has(client.user.id)) ? `<@!${client.user.id}>` : this.config.bot_config.prefix;
+  const isMention = (message.mentions.has(client.user.id)) ? `<@!${client.user.id}>` : config.prefix;
   const args = message.content.toString().slice(isMention.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
   if(command === "no") {
     message.channel.send("stop");
+  }
+
+  if(command === "1") {
+    message.guild.member(message.member).roles.add('827091381876555776');
+    message.react('👀')
   }
 });
