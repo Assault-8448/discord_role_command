08b5d5b90534ac3ae3e56f8c5061cd387b8e945d
Assault <vvvvvgaga12356@yahoo.co.jp>
2021-04-01 17:01:34 +0900

    b

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ client.on('ready', () => {
     console.log('準備完了！');
 });
 
+client.on('message', message => {
+  if (message.content === '-a') {
+    message.channel.send('E');
+  }
+});
+
 client.login('ODI3MDg2MzYzMjkwNTAxMTUy.YGV6Mw.LwcHYTXhjMcTzJmUEUYYxHBz458');
